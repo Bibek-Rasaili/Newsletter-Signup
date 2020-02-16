3c7c181fdee3e5cf2ce9e716ef2c624266ecda6f
Bibek Rasaili <Bibek.Rasaili7@gmail.com>
2020-02-16 09:37:52 +0000

    Images to public static folder

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
-const path = require('path');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static("public"));
+//specifies the static folder where all the static files are stored
+//they then refer relative to this folder ("public").  css/styles.css
 
 app.get("/", function(req, res){
   res.sendFile(__dirname+"/signup.html");
